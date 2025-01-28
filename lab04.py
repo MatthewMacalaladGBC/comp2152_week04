9b339d7fcfad98945e88bc2b5b2237c25b72d6f1
Matthew Macalalad <matthewmacalalad2012@gmail.com>
2025-01-28 11:43:24 -0500

    added lab04 question 6 code

--- lab04.py
+++ lab04.py
@@ -165,6 +165,14 @@ loot = loot_options.pop(lootRoll - 1)
 belt.append(loot)
 print("Your belt: " + belt)
 
+# Lab 04 - Question 6
+# Roll for and collect your second loot item
+input("Roll for the second item (Press enter)")
+lootRoll = random.choice(1, len(loot_options) + 1)
+loot = loot_options.pop(lootRoll - 1)
+belt.append(loot)
+print("Your belt: " + belt)
+
 input("Analyze the roll (Press enter)")
 # Compare Player vs Monster's strength
 print("--- You are matched in strength: " + str(combat_strength == m_combat_strength))
